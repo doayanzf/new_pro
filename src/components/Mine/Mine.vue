edc8e255b848c1fc1bfebf54104375ae25dc0ecb
YuuNaa <413752366@qq.com>
2018-01-31 12:43:43 +0800

    add rem

--- src/components/Mine/Mine.vue
+++ src/components/Mine/Mine.vue
@@ -1,10 +1,16 @@
 <template>
-    <div>
-       <h1>{{ msg }}</h1>
+<!-- 总背景图 -->
+    <div class="Dbody">
+        <div class="imgBack">
+            
+        </div>
     </div>
 </template>
     
 <script>
+
+
+
 export default {
     name: "component_name",
     data () {
@@ -16,5 +22,15 @@ export default {
 </script>
     
 <style lang="css" scoped>
-    
+    .Dbody{
+        width: 100%;
+        background-color: aquamarine;
+    }
+    .imgBack{
+        width: 100%;
+        height: 6.067708rem;
+        border: 1px red solid;
+        background: url('img/header_bg@3x.png');
+        background-size: cover;
+    }
 </style>
